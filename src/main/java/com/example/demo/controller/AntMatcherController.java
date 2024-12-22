a5a1cb9941a89d7fb603067404af26a3aab32233
jashan <jass200121@gmail.com>
2024-12-22 15:32:06 +0530

    add two functions

--- src/main/java/com/example/demo/controller/AntMatcherController.java
+++ src/main/java/com/example/demo/controller/AntMatcherController.java
@@ -5,12 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/public")
 public class AntMatcherController {
 
 	@GetMapping("/home")
 	public String home() {
-		return "this is home page";
+		return "this is home page";   
+	}
+	
+	@GetMapping("/log-in")
+	public String logIn() {
+		return "this is log in page";   
+	}
+	
+	@GetMapping("/register")
+	public String Register() {
+		return "this is resgister page";   
 	}
 	
 }
